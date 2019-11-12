b0411596cda91cfc43924c032f96a2ba5c52b43c
Alejandro Campos Magencio <alejacma@microsoft.com>
2019-11-12 14:28:58 +0100

    added job to databricks dsl cluster pipeline sample

--- pipelines/azuredatabricks-operator/databricks-dsl-cluster-pipeline.py
+++ pipelines/azuredatabricks-operator/databricks-dsl-cluster-pipeline.py
@@ -31,11 +31,45 @@ _CLUSTER_SPEC = """
 }
 """
 
+_JOB_SPEC = """
+{
+    "existing_cluster_id": "",
+    "libraries": [
+        {
+        "jar": "dbfs:/my-jar.jar"
+        },
+        {
+        "maven": {
+            "coordinates": "org.jsoup:jsoup:1.7.2"
+        }
+        }
+    ],
+    "timeout_seconds": 3600,
+    "max_retries": 1,
+    "schedule": {
+        "quartz_cron_expression": "0 15 22 ? * *",
+        "timezone_id": "America/Los_Angeles"
+    },
+    "spark_jar_task": {
+        "main_class_name": "com.databricks.ComputeModels"
+    }
+}
+"""
+
 def create_cluster(cluster_name, cluster_spec):
     return databricks.CreateClusterOp(
         name = "createcluster",
         cluster_name = cluster_name,
         spec = json.loads(cluster_spec)
+    )
+
+def create_job(cluster_id, job_name_prefix, job_spec):
+    spec = json.loads(job_spec)
+    spec["existing_cluster_id"] = cluster_id
+    return databricks.CreateJobOp(
+        name = "createjob",
+        job_name_prefix = job_name_prefix,
+        spec = spec
     )
 
 def delete_cluster(cluster_name):
@@ -48,9 +82,11 @@ def delete_cluster(cluster_name):
     name="DatabricksCluster",
     description="A toy pipeline that performs arithmetic calculations with a bit of Azure with Databricks.",
 )
-def calc_pipeline(cluster_name="test-cluster"):
+def calc_pipeline(cluster_name="test-cluster", job_name_prefix="test-job-"):
     create_cluster_result = create_cluster(cluster_name, _CLUSTER_SPEC)
-    delete_cluster(create_cluster_result.outputs["cluster_name"])
+    create_job_result = create_job(create_cluster_result.outputs["cluster_id"], job_name_prefix, _JOB_SPEC)
+    delete_cluster_result = delete_cluster(create_cluster_result.outputs["cluster_name"])
+    delete_cluster_result.after(create_job_result)
 
 if __name__ == "__main__":
     compiler.Compiler().compile(calc_pipeline, __file__ + ".tar.gz")
