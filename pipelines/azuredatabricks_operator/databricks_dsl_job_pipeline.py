7ca96051f32c89905a2367e86f7eb1e4a64a7c10
Alejandro Campos Magencio <alejacma@microsoft.com>
2019-12-02 10:43:07 +0100

    Now we can run a job in databricks dsl pipeline sample for jobs

--- pipelines/azuredatabricks_operator/databricks_dsl_job_pipeline.py
+++ pipelines/azuredatabricks_operator/databricks_dsl_job_pipeline.py
@@ -10,7 +10,7 @@ import kfp.dsl.databricks as databricks
     name="DatabricksRun",
     description="A toy pipeline that computes an approximation to pi with Azure Databricks."
 )
-def calc_pipeline(job_name="test-job", parameter="10"):
+def calc_pipeline(job_name="test-job", run_name="test-job-run", parameter="10"):
 
     # Sample based on https://docs.databricks.com/dev-tools/api/latest/examples.html#create-and-run-a-jar-job
     # Additional info:
@@ -26,20 +26,29 @@ def calc_pipeline(job_name="test-job", parameter="10"):
         },
         libraries=[{"jar": "dbfs:/docs/sparkpi.jar"}],
         spark_jar_task={
-            "main_class_name": "org.apache.spark.examples.SparkPi",
-            "parameters": [parameter]
-        },
-        schedule={
-            "quartz_cron_expression": "0 15 22 ? * *",
-            "timezone_id": "America/Los_Angeles"
+            "main_class_name": "org.apache.spark.examples.SparkPi"
         }
     )
+
+    create_run_task = databricks.SubmitRunOp(
+        name="submitrun",
+        run_name=run_name,
+        job_name=job_name,
+        jar_params=[parameter]
+    )
+    create_run_task.after(create_job_task)
+
+    delete_run_task = databricks.DeleteRunOp(
+        name="deleterun",
+        run_name=run_name
+    )
+    delete_run_task.after(create_run_task)
 
     delete_job_task = databricks.DeleteJobOp(
         name="deletejob",
         job_name=job_name
     )
-    delete_job_task.after(create_job_task)
+    delete_job_task.after(delete_run_task)
 
 if __name__ == "__main__":
     compiler.Compiler().compile(calc_pipeline, __file__ + ".tar.gz")
